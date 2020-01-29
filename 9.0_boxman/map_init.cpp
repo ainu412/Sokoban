dbfb55a0aa24f459dd8289725ada3876ed41f9b8
ainu412 <623287603@qq.com>
2020-01-29 17:50:09 +0800

    enum was added to optimize

--- 9.0_boxman/map_init.cpp
+++ 9.0_boxman/map_init.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <graphics.h>
 #include <Windows.h>
-//墙: 0，地板: 1，箱子目的地: 2,  小人: 3,	箱子: 4, 箱子命中目标: 5
+//墙: 0，地板: 1，箱子目的地: 2,  小人: MAN,	箱子: BOX, 箱子命中目标: HIT
 //舞台宽度960*768mm
 //舞台图片
 //loadimage 最后一个参数:是否是拉伸图片,true false差别不大
-IMAGE images[6];
-IMAGE bg_image;//bg-blackground-background
+
 #define ROW 9
 #define COLUMN 12
 #define SCREEN_WIDTH 1190
@@ -15,32 +14,45 @@ IMAGE bg_image;//bg-blackground-background
 #define START_CELLY (SCREEN_HEIGHT-ROW*CELL)/2
 #define CELL 61
 
-/*游戏地图*/
-int map[ROW][COLUMN] = {
-{   0,  0,  0,  0,  0,  0,  0,  0,  0,	0,  0,  0	},
-{   0,  1,  0,  1,  1,  1,  1,  1,  1,	1,  0,  0	},
-{	0,	1,	4,	1,	0,	2,	1,	0,	2,	1,	0,	0	},
-{	0,	1,	0,	1,	0,	1,	0,	0,	1,	1,	1,	0	},
-{	0,	1,	0,	2,	0,	1,	1,	4,	1,	1,	1,	0	},
-{	0,	1,	1,	1,	0,	3,	1,	1,	1,	4,	1,	0	},
-{	0,	1,	2,	1,	1,	4,	1,	1,	1,	1,	1,	0	},
-{	0,	1,	0,	0,	1,	0,	1,	1,	0,	0,	1,	0	},
-{	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0	},
+enum REFER{//refer-指代
+	WAL,//墙
+	FLR,//普通地板
+	DES,//目的地地板
+	MAN,//小人
+	BOX,//箱子
+	HIT,//到达目的地的箱子
+	ALL
 };
 
-int main1(){
+/*游戏地图*/
+int map[ROW][COLUMN] = {
+{   WAL,  WAL,   WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL	},
+{   WAL,  FLR,   WAL,  FLR,  FLR,  FLR,  FLR,  FLR,  FLR,  FLR,  WAL,  WAL	},
+{   WAL,  FLR,	 BOX,  FLR,  WAL,  DES,  FLR,  WAL,  DES,  FLR,  WAL,  WAL	},
+{   WAL,  FLR,   WAL,  FLR,  WAL,  FLR,  WAL,  WAL,  FLR,  FLR,  FLR,  WAL	},
+{   WAL,  FLR,   WAL,  DES,  WAL,  FLR,  FLR,  BOX,  FLR,  FLR,  FLR,  WAL	},
+{   WAL,  FLR,   FLR,  FLR,  WAL,  MAN,  FLR,  FLR,  FLR,  BOX,  FLR,  WAL	},
+{   WAL,  FLR,   DES,  FLR,  FLR,  BOX,  FLR,  FLR,  FLR,  FLR,  FLR,  WAL	},
+{   WAL,  FLR,   WAL,  WAL,  FLR,  WAL,  FLR,  FLR,  WAL,  WAL,  FLR,  WAL	},
+{   WAL,  WAL,   WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL,  WAL	},
+};
+
+IMAGE images[ALL];
+IMAGE bg_image;//bg-blackground-background
+
+int main(){
 	//搭台子
 	initgraph(SCREEN_WIDTH, SCREEN_HEIGHT);
 	loadimage(&bg_image,_T("blackground.bmp"), SCREEN_WIDTH, SCREEN_HEIGHT, true);
 	putimage(0, 0, &bg_image);
 
 	//加载格子图片
-	loadimage(&images[0], _T("wall.bmp"), CELL, CELL, true); 
-	loadimage(&images[1], _T("floor.bmp"), CELL, CELL, true); 
-	loadimage(&images[2], _T("des.bmp"), CELL, CELL, true); 
-	loadimage(&images[3], _T("man.bmp"), CELL, CELL, true); 
-	loadimage(&images[4], _T("box.bmp"), CELL, CELL, true); 
-	loadimage(&images[5], _T("box.bmp"), CELL, CELL, true);
+	loadimage(&images[WAL], _T("wall.bmp"), CELL, CELL, true); 
+	loadimage(&images[FLR], _T("floor.bmp"), CELL, CELL, true); 
+	loadimage(&images[DES], _T("des.bmp"), CELL, CELL, true); 
+	loadimage(&images[MAN], _T("man.bmp"), CELL, CELL, true); 
+	loadimage(&images[BOX], _T("box.bmp"), CELL, CELL, true); 
+	loadimage(&images[HIT], _T("box.bmp"), CELL, CELL, true);
 	for (int r=0; r<ROW; r++){
 		for (int c=0; c<COLUMN; c++){
 			putimage(START_CELLX+c*CELL, START_CELLY+r*CELL, &images[map[r][c]]);
